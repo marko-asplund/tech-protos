2d132d736a5a05ef153148b41ce76d172a1ca54d
marko asplund <marko.asplund@gmail.com>
2015-09-14 09:43:03 +0300

    load sparse matrix data

--- mllib-lda/src/main/scala/fi/markoa/proto/mllib/LDADemo.scala
+++ mllib-lda/src/main/scala/fi/markoa/proto/mllib/LDADemo.scala
@@ -5,42 +5,56 @@ import org.apache.spark.mllib.clustering.{LDA, DistributedLDAModel, LocalLDAMode
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.linalg.Vector
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
 
-class LDADemo {
+
+object Transformers {
+
+  def toSparseVectors(vocabularySize: Int, data: RDD[String]): RDD[Vector] = data.map { l =>
+    Vectors.sparse(vocabularySize, l.split(" ").drop(1).map { e =>
+      e.split(":") match {
+        case Array(id, cnt) => (id.toInt, cnt.toDouble)
+      }
+    })
+  }
+
+}
+
+class LDADemo(numberOfTopics: Int, vocabularySize: Int) {
+  val logger = Logger(LoggerFactory.getLogger(classOf[LDADemo]))
   val AppName = "myApp"
   val Master = "local"
-  val DataFileName = "data/sample_lda_data.txt"
+  val DataFileName = "data/data-head50.txt"
   val ModelFileName = "myLDAModel"
-  val TopicCount = 3
 
   val ctx = new SparkContext(new SparkConf().setAppName(AppName).setMaster(Master))
 
   def trainModel = {
-    // Load and parse the data
-    val data = ctx.textFile(DataFileName)
+    val parsedData = Transformers.toSparseVectors(vocabularySize, ctx.textFile(DataFileName))
 
-    val parsedData = data.map(s => Vectors.dense(s.trim.split(' ').map(_.toDouble)))
-    // Index documents with unique IDs
     val corpus = parsedData.zipWithIndex.map(_.swap).cache()
 
-    // Cluster the documents into three topics using LDA
-    val ldaModel = new LDA().setK(TopicCount).run(corpus)
+    logger.info("LDA training: start")
+    val ldaModel = new LDA().setK(numberOfTopics).run(corpus)
+    logger.info("LDA training: done")
 
-    // persist model
     ldaModel.save(ctx, ModelFileName)
+    logger.info("model saved")
 
     ldaModel
   }
 
-  def getPrediction(docNum: Int) = {
+  def getPrediction(input: String) = {
+    // NB: don't load model for each prediction in non-proto code.
+    logger.info("load model: start")
     val dm = DistributedLDAModel.load(ctx, ModelFileName)
     val model = dm.toLocal
+    logger.info("load model: done")
 
-    val rawData = ctx.textFile(DataFileName).collect
-    val data = Seq((0L, Vectors.dense(rawData(docNum).trim.split(' ').map(_.toDouble))))
-    val samples = ctx.makeRDD(data)
-    val r = model.topicDistributions(samples)
-
+    val samples = Transformers.toSparseVectors(vocabularySize, ctx.parallelize(Seq(input)))
+    val r = model.topicDistributions(samples.zipWithIndex.map(_.swap))
+    logger.info("topicDistributions: done")
     (dm, r)
   }
 
@@ -63,7 +77,7 @@ class LDADemo {
     // Output topics. Each is a distribution over words (matching word count vectors)
     println("Learned topics (as distributions over vocab of " + ldaModel.vocabSize + " words):")
     val topics = ldaModel.topicsMatrix
-    for (topic <- Range(0, TopicCount)) {
+    for (topic <- Range(0, numberOfTopics)) {
       print("Topic " + topic + ":")
       for (word <- Range(0, ldaModel.vocabSize)) { print(" " + topics(word, topic)); }
       println()
@@ -76,13 +90,11 @@ class LDADemo {
 object LDADemo {
 
   def main(args: Array[String]): Unit = {
-    val l = new LDADemo
+    val l = new LDADemo(100, 105000)
     if (args.length > 0 && args(0) == "train") {
       val m = l.trainModel
-      l.dumpTopicsMatrix(m)
     } else {
       val p = l.getPrediction(5)
-      l.dumpTopicsAndPrediction(p)
     }
     l.stop
   }
